c74b2a4e0105b16a7b895bc70e26111c22a77fbc
Raymund <raymund.balicante@freshclinics.com>
2025-10-19 15:56:25 +0800

    feat: added vercel speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import "./globals.css";
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en" className={inter.className}>
       <body>{children}</body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
